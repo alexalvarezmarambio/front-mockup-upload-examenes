a0a1fb7426d1e8fb4b050e83575728625ab09b33
Alex Alvarez <alexalvarezmarambio@gmail.com>
2025-09-20 21:01:44 -0300

    mockup terminado

--- src/app/pages/upload-file/upload-file.component.ts
+++ src/app/pages/upload-file/upload-file.component.ts
@@ -1,11 +1,53 @@
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatMiniFabButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-upload-file',
-  imports: [],
+  imports: [
+    MatIcon,
+    MatTableModule,
+    DatePipe,
+    MatMiniFabButton
+  ],
   templateUrl: './upload-file.component.html',
   styleUrl: './upload-file.component.scss'
 })
 export class UploadFileComponent {
+  files: File[] = [];
+  dataSource!: MatTableDataSource<File>;
+  columnas = ['document', 'date', 'size', 'action'];
 
+  uploadFile(event: Event) {
+    const input = event.target as HTMLInputElement;
+
+
+    if (input.files && input.files.length > 0 ) {
+      this.files.push(...input.files);
+      this.dataSource = new MatTableDataSource(this.files);
+    }
+
+    input.value = '';
+  }
+
+  mapSize(size: number) {
+    const result = Math.round((size / 1024));
+    if (result < 1000) {
+      return `${result} KB`;
+    } else {
+      return `${Math.round(result / 1024)} MB`
+    }
+  }
+
+  delete(index: number) {
+    this.files.splice(index, 1);
+    this.dataSource = new MatTableDataSource(this.files);
+  }
+
+  getTotal() {
+    const total = this.files.reduce((accu, value) => accu + value.size, 0);
+    return this.mapSize(total);
+  }
 }
